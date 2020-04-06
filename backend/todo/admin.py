52cf23611d04245a3411477dc46d400678b4d9fc
Katherine Kampf <kkampf@umich.edu>
2020-04-05 23:04:32 -0400

    setup admin and cors middleware

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description','completed')
+
+admin.site.register(Todo, TodoAdmin)
