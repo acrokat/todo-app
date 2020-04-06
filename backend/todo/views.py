4f7ffad12f3bcebc567e08bf09d664f7c114138e
Katherine Kampf <kkampf@umich.edu>
2020-04-05 23:56:40 -0400

    temp

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    quaryset = Todo.objects.all()
 
 # Create your views here.
